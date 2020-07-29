2b7beaa3d965f6a0bc32977253a1b6c04bec3128
phuchealer91 <pepegvcn2@gmail.com>
2020-07-29 21:45:07 +0700

    update style

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,36 @@
-import Slider from '/Slider'
-import 'public/css/index.css'
+import * as React from 'react'
+import Swiper from 'react-id-swiper'
+import './dist/index.css'
+export interface IProps {
+  children?: React.ReactNode
+}
+export const Slider = ({ children }: IProps) => {
+  const params = {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 1,
+      },
+      320: {
+        slidesPerView: 1,
+      },
+    },
+  }
 
-export default Slider
+  return <Swiper {...(params as any)}>{children}</Swiper>
+}
--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import Slider from '../src/Slider'
+import { Slider } from '../src/Slider'
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
